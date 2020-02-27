dc6008e796e6287285b17bd00266de02728db2df
Cyber Kommando <cyberubenex@hotmail.com>
2020-02-27 11:42:42 +0000

    VariasStrings

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "É uma string";
+            string b = "outra string";
+            string c = @"Isto é um codigo em verbatim";
+            string d = @"""Esta é uma string entre tres aspas""";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+        }
+    }
+}
