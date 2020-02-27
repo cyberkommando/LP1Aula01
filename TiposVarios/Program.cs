3c0d7f6a0978f478e55d1260b6ed0fa219bad972
Cyber Kommando <cyberubenex@hotmail.com>
2020-02-27 11:20:49 +0000

    TiposVarioschar

--- TiposVarios/Program.cs
+++ TiposVarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace TiposVarios
 {
@@ -6,21 +7,26 @@ namespace TiposVarios
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             int x = 7;
             uint y = 7U;
             long z = 7L;
             ulong a = 7UL;
             int b = 0x10;
             uint c = 0x10U;
-            int d =0b10110110;
+            int d = 0b10110110;
+            char e = '\u0065';
 
-            Console.Writeline(x);
-            Console.Writeline(y);
-            Console.Writeline(z);
-            Console.Writeline(a);
-            Console.Writeline(b);
-            Console.Writeline(c);
-            Console.Writeline(d);
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            
         }
     }
 }
