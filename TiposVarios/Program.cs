17c63d2343b4a03741bb16bf396187603bcbabbe
Cyber Kommando <cyberubenex@hotmail.com>
2020-02-27 10:59:56 +0000

    TiposVarios

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 7;
+            uint y = 7U;
+            long z = 7L;
+            ulong a = 7UL;
+            int b = 0x10;
+            uint c = 0x10U;
+            int d =0b10110110;
+
+            Console.Writeline(x);
+            Console.Writeline(y);
+            Console.Writeline(z);
+            Console.Writeline(a);
+            Console.Writeline(b);
+            Console.Writeline(c);
+            Console.Writeline(d);
+        }
+    }
+}
